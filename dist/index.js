b60e6ddcdb911e1a9b2d1ab463c8304509bc1309
VolodymyrKravchenko <109697703+AscariA10@users.noreply.github.com>
2023-05-04 13:31:08 +0300

    typeScript practice with compiler

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const myNumber = 1;
+const myString = 'sfdsfasdfasdfasd';
+const myObject = { a: 10, d: 'asdfsf', f: true };
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const myNumber: number = 1;
+
+const myString: string = 'sfdsfasdfasdfasd';
+
+//comment about my project
+
+const myObject: {} = { a: 10, d: 'asdfsf', f: true };
